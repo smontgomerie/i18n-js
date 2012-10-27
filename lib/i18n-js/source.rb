21f683cd3a5aa5770ba4d7cdbec8d117bfde7a62
Joel Jackson <jackson.joel@gmail.com>
2012-10-26 17:10:17 -0700

    fixed the source path

--- lib/i18n-js/source.rb
+++ lib/i18n-js/source.rb
@@ -2,7 +2,7 @@ module SimpleIdeias
   module I18n
     module Source
       def self.bundled_path
-        File.expand_path("../../vendor/assets/javascripts/i18n.js", __FILE__)
+        File.expand_path("../../../vendor/assets/javascripts/i18n.js", __FILE__)
       end      
     end
   end
